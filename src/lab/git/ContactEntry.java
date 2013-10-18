8c1530cff9d92bfa2c3db6655c525b5314b7b37b
kessir <m.kessir.adjaho@gmail.com>
2013-10-18 15:35:05 +0000

    Form and data structure ready

---
+++ src/lab/git/ContactEntry.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab.git;
+
+/**
+ *
+ * @author kessir
+ */
+public class ContactEntry {
+    private String name;
+    private String phoneNumber;
+
+    public ContactEntry(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+    
+    
+    
+}
